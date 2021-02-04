33fd0068abd1f0ae7d04569421891193ce41185c
陈永鸿 <chenyonghong@chenyonghongdeMacBook-Pro.local>
2021-02-04 10:51:08 +0800

    更新首页文档

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,17 @@ module.exports = {
   runtimeCompiler: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-element-components/'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+    config.module.rule('md')
+      .test(/\.md/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-markdown-loader')
+      .loader('vue-markdown-loader/lib/markdown-compiler')
+      .options({
+        raw: true
+      })
+  }
 }
